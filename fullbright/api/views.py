0f1a2dc1e28ae6165dd803c116b1a800febfad36
ghecharaf <ghecharaf@gmail.com>
2023-01-25 16:54:29 +0100

    debut

--- fullbright/api/views.py
+++ fullbright/api/views.py
@@ -3003,6 +3003,7 @@ class PigeFinaleAdminArticleView(generics.ListAPIView):
                 'tarifsec': tarif[0].prix if tarif != ''else '/'
             })
             i += 1
+        response = sorted(response, key=lambda d: d['debut'])
         response = sorted(response, key=lambda d: d['date'])
         response = sorted(response, key=lambda d: d['support'])
 
